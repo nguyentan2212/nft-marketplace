c78e8650c1e998c567962cc2d94875c4963908e6
nguyentan2212 <nguyentan2212@gmail.com>
2022-02-21 16:12:23 +0700

    remove access control

--- src/contracts/contracts/NFTControl.sol
+++ src/contracts/contracts/NFTControl.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./NFT721.sol";
 
-contract NFTControl is AccessControl {
-
-    bytes32 public constant REGISTRY_ROLE = keccak256("REGISTRY_ROLE");
+contract NFTControl {
     NFT721 private immutable _implementation;
 
     struct NFTs {
         // E.g. if `lastestNFT == 2`, there are 2 `NFT` contracts deployed.
         uint256 numOfContracts; 
-        // Mapping from index => nft contract address.
-        mapping(uint256 => address) NFTAddress;
+        // Mapping from index => nft contract address, start from 1
+        mapping(uint256 => address) NFTAddresses;
     }
 
     /// @dev Mapping from app deployer => index and app addresses.
@@ -23,19 +19,27 @@ contract NFTControl is AccessControl {
     event DeployedContract(address indexed _deployer, address indexed contractAddress);
     constructor() {
         _implementation = new NFT721();
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function deployNFT(string memory name, string memory symbol,string memory uri) 
+    function deployNFT(string memory name, string memory symbol,string memory uri, uint96 royalty) 
     external 
     returns (address contractAddress) {
-        address deployer = _msgSender();
+        address deployer = msg.sender;
         uint256 nonce = getNextIndex(deployer);
         bytes32 salt = keccak256(abi.encodePacked(deployer, nonce));
         contractAddress = Clones.cloneDeterministic(address(_implementation), salt);
-        NFT721(contractAddress).initialize(name, symbol, uri, deployer);
+        NFT721(contractAddress).initialize(name, symbol, uri, deployer, royalty);
 
         emit DeployedContract(deployer, contractAddress);
+    }
+
+    function getAllContractOfUser(address user) external view returns (address[] memory contracts) {
+        uint256 numOfContracts = _nfts[user].numOfContracts;
+        contracts = new address[](numOfContracts);
+
+        for (uint256 i = 1; i <= numOfContracts; i++) {
+            contracts[i-1] =  _nfts[user].NFTAddresses[i];
+        }
     }
 
     function getNextIndex(address deployer) internal returns (uint256) {
