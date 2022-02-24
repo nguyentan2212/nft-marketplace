55c1eaaa0843b32975bd3edb8dc3b9d2b9a2d7d2
nguyentan2212 <nguyentan2212@gmail.com>
2022-02-24 16:00:53 +0700

    index for views

---
+++ src/views/index.js
@@ -0,0 +1,4 @@
+export { default as Admin } from "./Admin";
+export { default as Marketplace } from "./Marketplace";
+export { default as NFTBalance } from "./NFTBalance";
+export { default as UserDashboard } from "./UserDashboard";
