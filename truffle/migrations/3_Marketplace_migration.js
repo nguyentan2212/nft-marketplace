78ca6f1256e7039fe8eddcd4d755b5f95377fa9c
nguyentan2212 <nguyentan2212@gmail.com>
2022-03-01 22:10:43 +0700

    [chore] remove token.mint

--- truffle/migrations/3_Marketplace_migration.js
+++ truffle/migrations/3_Marketplace_migration.js
@@ -3,12 +3,7 @@
 const Marketplace = artifacts.require("Marketplace");
 const MyToken = artifacts.require("MyToken");
 
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function (deployer) {
   deployer.deploy(Marketplace);
-  await deployer.deploy(MyToken);
-  const token = await MyToken.deployed();
-  
-  await token.mint(accounts[0], 1000000, { from: accounts[0]});
-  await token.mint(accounts[1], 1000000, { from: accounts[0]});
-  await token.mint(accounts[2], 1000000, { from: accounts[0]});
+  deployer.deploy(MyToken); 
 };
