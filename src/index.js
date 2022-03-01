60fd6a8d465772bfcf276806ca3bbb705bf2a15a
nguyentan2212 <nguyentan2212@gmail.com>
2022-03-01 22:07:09 +0700

    [refactor] remove console.log

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "./index.css";
 
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
-
 const Application = () => {
     const isServerInfo = !!(APP_ID && SERVER_URL);
     if (isServerInfo)
