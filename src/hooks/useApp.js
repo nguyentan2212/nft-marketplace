62270e0cc8399d9592be67c719eaaba0c86b9978
nguyentan2212 <nguyentan2212@gmail.com>
2022-03-04 17:25:09 +0700

    [chore] get data from useMarketplace hook

--- src/hooks/useApp.js
+++ src/hooks/useApp.js
@@ -1,10 +1,12 @@
 import useMarketplace from "./useMarketplace.js";
+import { useMoralis } from "react-moralis";
 
 function useApp() {
     const { marketplaceAddress } = useMarketplace();
-    const hasMarketplace = true;
+    const { account } = useMoralis();
+    const hasMarketplace = marketplaceAddress ? true : false;
     const canSetProject = true;
-    const AdminAddress = "admin";
+    const AdminAddress = account;
     const isFetching = true;
     return { marketplaceAddress, hasMarketplace, canSetProject, AdminAddress, isFetching };
 }
